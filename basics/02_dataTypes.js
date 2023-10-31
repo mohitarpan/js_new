f83fbce87d76a79da8139d3fdfeeeefb8129a3de
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2023-10-31 20:04:44 +0000

    datatypes

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+ "use Strict" // it is use for the js new code 
+// Name of datatype                                             
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value data is object 
+//undefined =>
+//symbol >> unique used 
+
+// object 
+console.log(typeof null);// o/p underfine
+console.log(typeof null);// o/p object 
