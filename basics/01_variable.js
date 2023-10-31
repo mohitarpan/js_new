559a588b308d79502b6de53877c13106b530e888
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2023-10-31 20:05:32 +0000

    variables

---
+++ basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmial = "mohitarpan.01@gmial.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+ 
+// accountId = 2// not Allowed
+console.log(accountId);
+console.table([accountEmial,accountId,accountPassword,accountCity])
+/*
+prefer not to use var 
+because of issue in block scope and funtional scope 
+//
+console.table([]) is is use to put variable together in tabular form as an output.
+
+
+
+*/
