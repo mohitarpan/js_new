e664344338b011d1e4ec90e596bd685de32e2fdc
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2023-11-07 13:05:00 +0000

    operations

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = null
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)// It convert string but did not print number it print Nan
-console.log(typeof valueInNumber)
-console.log(valueInNumber)// its shows a Nan(not a number ) is
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)// its shows a Nan(not a number ) is
 
 //  "33" => 33
 //  "3abc" => Nan
@@ -13,12 +13,32 @@ console.log(valueInNumber)// its shows a Nan(not a number ) is
 
 let isloggedIn = "null"
 let bolleanIsLoggedIn = Boolean(isloggedIn)
-console.log(typeof bolleanIsLoggedIn)
-console.log(bolleanIsLoggedIn)
+// console.log(typeof bolleanIsLoggedIn)
+// console.log(bolleanIsLoggedIn)
 //  1=>  true ; 0=> false
 // "" => false; "anything" => true
 
 let someNumber = 33
 let stringNumber = String (someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+// ***************************************** operation ******************************
+
+/* 
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**3)
+
+*/
+let str1 = "hello "
+let str2 = "namste"
+// console.log(str1 + str2)
+// console.log("1" + 2)
+// console.log(1 + "2");
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 +"3")
+console.log(+trur) // not a write way to write a code 
+// output is 1 
