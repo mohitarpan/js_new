6d1b102bc67d5b1306fe2450b4b4062c1a9dcb9a
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2023-12-23 17:56:03 +0000

    number

---
+++ basics/06_number_and_maths.js
@@ -0,0 +1,15 @@
+const score = 400
+console.log(score);
+// when we use new Number where we define the type of value
+// 
+const balance = new Number(100.999)
+console.log(balance);
+
+console.log(balance.toString().length);//find the lenth of string 
+console.log(balance.toFixed(2));// with this we fixed the precision value or decimal value 
+
+const otherNumber = 23.8766
+console.log(otherNumber.toPrecision(4));
+
+
+const hundred
