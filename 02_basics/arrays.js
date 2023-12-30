3c4aed803400ebf6d6668ef08867b6146d9c8a6e
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2023-12-30 20:49:01 +0000

    Starting of Array with few methods

---
+++ 02_basics/arrays.js
@@ -0,0 +1,8 @@
+//  Array
+const myArr = [0,1,2,3,4,5]
+console.log(myArr[2]);
+const myHeores = ['saktimaan','Batman']
+
+
+const myArr2 = new Array(1,2,3,4,5) // it is an object type decleration of Array still get or store the value like array this the beauty of objects
+
