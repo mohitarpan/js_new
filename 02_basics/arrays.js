15ca2468486d975c613924fffcf47d18ba203253
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-01-01 18:36:06 +0000

    ohh 1st time

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -5,4 +5,5 @@ const myHeores = ['saktimaan','Batman']
 
 
 const myArr2 = new Array(1,2,3,4,5) // it is an object type decleration of Array still get or store the value like array this the beauty of objects
-
+// doing just for streck file
+ 
