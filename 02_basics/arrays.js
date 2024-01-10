8a691ada87ca49478fe3455decd0489925a10982
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-01-10 14:03:35 +0000

    done with part 1 Array

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -17,6 +17,17 @@ const myArr2 = new Array(1,2,3,4,5)
 // console.log(myArr.includes(9));//it gives false because it was not in the array.
 // console.log(myArr.indexOf(9));// if element was not present in Array it shows -1 (means he did not know)
 
-const newArr = myArr.join()
-console.log(myArr);
-console.log(newArr);
+// const newArr = myArr.join()// it changes the type of string and bind toghter as a Maths .
+// console.log(myArr);
+// console.log(newArr);
+
+// slice , splice
+console.log("A" ,myArr);
+const myn1 = myArr.slice(1,3);// it does not manuplate the Orginal Array
+
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,3);// when we use splice it include the range but also "change the orginal array."
+console.log(myn2);
+console.log("C",myArr);
