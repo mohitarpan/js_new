44f15d745b1ed8a1563ac02ef72aa96d3e202e76
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-01-14 18:00:48 +0000

    freez method for objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -31,3 +31,8 @@ console.log(jsUser.email);
 console.log(jsUser["email"]);
 console.log(jsUser["full name"]);// here . fullname is not able to access but in square brackit it will take or fetch the value 
 console.log(jsUser[mySym]);//when we did not use "" it will shows undefine 
+
+jsUser.email ="arpanyadav.01@gmail.com"
+Object.freeze(jsUser)
+jsUser.email ="1900300100044@ipec.org.in"// here after freezing the object it will not update the value so it will so arpanyadav.01@gmail.com
+console.log(jsUser);
