bd9675541a4273624420ae259febcdeb38ac2387
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-01-20 19:08:18 +0000

    start with function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,11 @@
+ // Function 
+
+function sayMyName() {
+    console.log("A");
+    console.log("R");
+    console.log("P");
+    console.log("A");
+    console.log("N");
+} 
+// sayMyName // it is called reference(function lived there we just pointing out about function )
+sayMyName() // execution of function 
