ad09fb8a7b1a42e5432c77b6d2808c842b801ec4
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-01-23 12:55:36 +0000

    done with scopes

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -40,9 +40,24 @@ if(true) {
     const username = "yadav"
     if(username === "yadav") {
         const website = " arpanport"
-        console.log(username + website);
+        // console.log(username + website);
     }
     // console.log(website);// we can not access the website variable out of the scope it is declared in nested if we commented even this whole is showing
     // beacuse username is also out of the scope of first if 
 }
 // console.log(username);
+
+//   +++++++++++++++++++ intresting concept ++++++++++++++++++++++++++++++
+// Hoisting
+console.log(addone(5))
+function addone(num) {
+    return num + 1   
+}
+// it is executed beacuse of hoisting here exection contetxt is create for whole function 
+// it is also depend on decleration of function just like above we have hoisting but in variable declearion d=function we can not able to use 
+console.log(addTwo(5)); // here we have
+const addTwo = function(num) { // here function decleration with holding into a variable 
+    return num + 2 
+} 
+
+//caution : variable are very powerful in js it can hold any thing letrally any thing  
