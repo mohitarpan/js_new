229879eb65e8848773e2035d724915d5a7584e50
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-01-28 03:43:59 +0000

    immediate Invoked functions

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -6,10 +6,16 @@
 // chai() //it is not iife so what we have to for iife
 
 (function chai(){
+    // namede IIFE
     console.log(`DB Connected`);
-})()
+})();
 
-()() // it is first for the functions defination  and second is execution call
+// ()() // it is first for the functions defination  and second is execution call
 // jo function immediately execute ho jae (ok not for interview)
 //what is iife
 // Due to the pollution(variable and declaration) of global scope we used iife to remove them 
+((name) =>  {
+    console.log(`DB Connected TWo ${name}`);
+})('Aepan')
+// it gives an error so we need a semicoulm 
+// it have that property iife dont know where to end the function without that we can not predicts that function can be ended.
