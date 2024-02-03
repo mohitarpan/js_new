8b3b9c4ad466632b9ed2b4842e4c3dd01c72740d
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-02-03 18:32:49 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,17 @@
+// if 
+// if(conditions) {
+
+// }
+// const isUserloggedIn = true
+
+// if( 2 === '2'){ // === is also check the data type of varialbe.
+//     console.log("executed");
+// }
+// console.log("Not_executed");
+
+const isUserloggedIn = true
+
+if( 2 === '2'){ // === is also check the data type of varialbe.
+    console.log("executed");
+}
+console.log("Not_executed");
