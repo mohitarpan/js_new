dcdd6d992f3503262448d461d16778c0c33a8bd4
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-02-03 19:40:44 +0000

    Switch pafrt two

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,37 @@
+// ---------------------------- Basic Syntax-------------------
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const month = 4
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        // break; // if break is commented it does not match(execute) the remaing case except default value 
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    default:
+        console.log("Default case match");
+        break;
+}
+// break is used to break a control flow of switch and 
