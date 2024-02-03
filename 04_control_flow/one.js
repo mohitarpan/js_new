2e770915e28b83a1e49d3a7d258b2863f8b2cf9c
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-02-03 18:59:51 +0000

    short hand notation of if

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -2,16 +2,32 @@
 // if(conditions) {
 
 // }
-// const isUserloggedIn = true
+const isUserloggedIn = true
 
 // if( 2 === '2'){ // === is also check the data type of varialbe.
 //     console.log("executed");
 // }
 // console.log("Not_executed");
 
-const isUserloggedIn = true
+// difference btw !=  and !== that this is also check the data type of condtion in if statements like '1' != 1 here condtion is false but '1' !== here contion is true  
+// if( 2 !== '2'){ // 
+//     console.log("executed");
+// }
+// else
+// console.log("Not_executed");
 
-if( 2 === '2'){ // === is also check the data type of varialbe.
-    console.log("executed");
-}
-console.log("Not_executed");
+// ---------------------------Block scope--------------------
+// const score = 200
+// if(score > 100) {
+//     const power = "fly"
+//     console.log(`user Power: ${power}`);
+// }
+// console.log(`user Power: ${power}`);
+
+//  how 'if' short hand notation is done 
+ const  balance = 1000
+// if (balance > 500) console.log("test"),// Mostly in short hand notation line must be written in single line it does not allow multiple like "usually" 
+// console.log("test2for ,");
+//  u can use , to perfoem multiple line of code in this but it is not good practice 
+
+
