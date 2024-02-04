09da22a79f9c9b655d760cfa2d67da75a1ec556e
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-02-04 20:10:41 +0000

    complete control flow

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -31,4 +31,10 @@ let val1;
 // val1 = null ?? undefined
 // val1 = null ?? 20 ?? 30 // it gives the first value except null
 val1 = ""?? null//empty value but work 
-console.log(val1);
+// console.log(val1);
+// ternory operator and nullish coalescing operator
+
+//  Terniary operator
+// condition ? true : false
+const iceTeaprice = 100
+iceTeaprice <= 80 ? console.log("less than 80"): console.log("morethan 80");;
