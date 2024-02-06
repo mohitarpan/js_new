4823949888c5862507a4380c84d7776d13974346
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-02-06 10:44:47 +0000

    use of break

--- 05_itteration/one.js
+++ 05_itteration/one.js
@@ -9,10 +9,33 @@ for (let i = 0; i <10; i++) {
 }
 
 for (let i = 1; i <10; i++) {
-    console.log(`outer loop values: ${i}`);
+    // console.log(`outer loop values: ${i}`);
     for (let j = 1; j <10; j++) {
-        console.log(i + '*' + j +' = ' + i*j);
+       // console.log(i + '*' + j +' = ' + i*j);
         
     }
     
 }
+let myArray =["flash","batman","superman"]
+// console.log(myArray.length);
+for (let o = 0; o < myArray.length; o++) {
+    const element = myArray[o];
+    // console.log(element);
+    
+}
+//  
+for (let index = 0; index < 20; index++) {
+    if( index == 5){
+        console.log(`Detected 5`);
+        break// one break is executed it will breact the floe and comes out of the for loop and control reaches to line no 34
+    }
+    console.log(`value of i is ${index}`);
+}
+for (let index = 0; index < 20; index++) {
+    if( index == 5){
+        console.log(`Detected 5`);
+        continue
+    }
+    console.log(`value of i is ${index}`);
+}
+
