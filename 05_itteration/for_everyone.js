8d90f194394fde7ab4ae5723a3c9660a7c3adf54
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-02-10 20:21:53 +0000

    foreach function for objects in an array

--- 05_itteration/for_everyone.js
+++ 05_itteration/for_everyone.js
@@ -9,7 +9,32 @@ const coding =["js","ruby","java","python","cpp","bhailang"]
 // coding.forEach( (val) =>{
 //     console.log(val);
 // })
-function printme(value) {
-    console.log(value);
-}
-coding.forEach(printme)// we just need to pass the function as argument no need to execute 
+// function printme(value) {
+//     console.log(value);
+// }
+// coding.forEach(printme)// we just need to pass the function as argument no need to execute 
+
+
+//  coding.forEach((item,index,arr)=>{ // it does not hold only itemor val arguments it also give us index and about an array 
+
+//     console.log(item,index,arr);
+//  })
+const mycoding =[
+    {
+        languagename:"javscript",
+        lnhuagefilename:"js"
+    },
+    {
+        languagename:"java",
+        lnhuagefilename:"java"
+    },
+    {
+        languagename:"python",
+        lnhuagefilename:"py"
+    },
+]
+mycoding.forEach((item) =>{
+    // console.log(item.languagename);/// here we can fatch the objects from array using forEach loop
+})
+
+const new = Map
