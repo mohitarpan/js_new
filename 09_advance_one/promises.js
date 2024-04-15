55f3d6dcaf9517065b71921f3abb31de1dc79596
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-04-15 14:10:14 +0000

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -16,3 +16,30 @@ const promiseOne = new Promise(function(resolve,reject){
 promiseOne.then(function(){
     console.log("Promise Consumed");
 })
+
+// Advance promises
+new Promise(function(resolve,reject){
+    setTimeout(() => {
+        console.log("Async task 2");
+        resolve()
+    }, 1000);
+})
+.then(function(){
+    console.log("Async @ resolve")
+})
+
+const promiseThree = new Promise(function (resolve,reject) 
+{
+    setTimeout(function(){
+        resolve({username: "Chai", email: "chai@example.com"})
+    },1000)    
+})
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+constpromiseFour = new Promise
+
+// -----------------------  Dictonary ------------------------
+// callback hell : callback into callback into callback it is also reduce by promise
+///
