b2b40eea4d1fb993ac4beb653d85f7f0f93867f9
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-04-21 20:22:37 +0000

    promise last

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -38,7 +38,75 @@ promiseThree.then(function(user){
     console.log(user);
 })
 
-constpromiseFour = new Promise
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false;
+        if(!error){
+            resolve({username:"Arpan",password: "123"})
+        } else {
+            reject('ERROR: Something went Wrong')
+        }
+    },1000)
+})
+// we can not access user name directly by storing promisFour into constant..
+// const username = promiseFour.then(() =>{}) It will doest not give Arpan output 
+promiseFour
+.then((user)=>{
+    console.log(user);
+    return user.username
+}).then((username)=> {
+    console.log(username);
+}).catch(function(Error) {
+    console.log(Error);
+    
+}).finally(()=> {
+    console.log("The promise is resolved or rejeced ");// it will work either then or catch not grab he promises promies did not wait for more than their thresholde
+})
+
+
+const promiseFive = new Promise((function(resolve,reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username:"sabri",password: "879"})
+        } else {
+            reject('ERROR: sabri did not respond Wrong')
+        }
+    },1000)
+}));
+
+async function consumedPromiseFive(){
+    try{
+        const respond = await promiseFive
+        console.log(respond);
+    }catch(error) {
+        console.log(error)
+    }
+}
+consumedPromiseFive()
+
+async function getUserAll(){
+    const respond = await fetch('')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // -----------------------  Dictonary ------------------------
 // callback hell : callback into callback into callback it is also reduce by promise
